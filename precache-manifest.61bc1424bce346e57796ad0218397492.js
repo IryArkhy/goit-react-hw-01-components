9c44dfb855b6ba58e672e9a45343534297a89eda
Iryna <37odri@gmail.com>
2019-10-15 22:51:44 +0300

    Updates

---
+++ precache-manifest.61bc1424bce346e57796ad0218397492.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c94620ed443a30119e1b1efba697678a",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "0584a05428c7ab7b8c09",
+    "url": "/goit-react-hw-01-components/static/css/main.e9c7a3ae.chunk.css"
+  },
+  {
+    "revision": "bf4904bbf31b04edeb94",
+    "url": "/goit-react-hw-01-components/static/js/2.bcd4db2f.chunk.js"
+  },
+  {
+    "revision": "0584a05428c7ab7b8c09",
+    "url": "/goit-react-hw-01-components/static/js/main.b2460713.chunk.js"
+  },
+  {
+    "revision": "1deac508ae55ff559187",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.a749bb14.js"
+  }
+]);
